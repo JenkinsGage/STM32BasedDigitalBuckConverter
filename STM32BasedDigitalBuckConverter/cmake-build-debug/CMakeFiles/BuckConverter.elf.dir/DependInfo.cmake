1636f83f401a604f039d1d563aa9236914436000
Jenkins Gage <indiegd.jenkinsgage@gmail.com>
2021-11-30 15:41:12 +0800

    Initial commit

---
+++ STM32BasedDigitalBuckConverter/cmake-build-debug/CMakeFiles/BuckConverter.elf.dir/DependInfo.cmake
@@ -0,0 +1,79 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "ASM"
+  "C"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_ASM
+  "E:/Projects/Embed/STM32/STM32BasedDigitalBuckConverter/STM32BasedDigitalBuckConverter/Startup/startup_stm32f103c6tx.s" "E:/Projects/Embed/STM32/STM32BasedDigitalBuckConverter/STM32BasedDigitalBuckConverter/cmake-build-debug/CMakeFiles/BuckConverter.elf.dir/Startup/startup_stm32f103c6tx.s.obj"
+  )
+set(CMAKE_ASM_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_ASM
+  "DEBUG"
+  "STM32F103x6"
+  "USE_HAL_DRIVER"
+  )
+
+# The include file search paths:
+set(CMAKE_ASM_TARGET_INCLUDE_PATH
+  "../Inc"
+  "../Drivers/STM32F1xx_HAL_Driver/Inc"
+  "../Drivers/STM32F1xx_HAL_Driver/Inc/Legacy"
+  "../Drivers/CMSIS/Device/ST/STM32F1xx/Include"
+  "../Drivers/CMSIS/Include"
+  )
+set(CMAKE_DEPENDS_CHECK_C
+  "E:/Projects/Embed/STM32/STM32BasedDigitalBuckConverter/STM32BasedDigitalBuckConverter/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal.c" "E:/Projects/Embed/STM32/STM32BasedDigitalBuckConverter/STM32BasedDigitalBuckConverter/cmake-build-debug/CMakeFiles/BuckConverter.elf.dir/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal.c.obj"
+  "E:/Projects/Embed/STM32/STM32BasedDigitalBuckConverter/STM32BasedDigitalBuckConverter/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_cortex.c" "E:/Projects/Embed/STM32/STM32BasedDigitalBuckConverter/STM32BasedDigitalBuckConverter/cmake-build-debug/CMakeFiles/BuckConverter.elf.dir/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_cortex.c.obj"
+  "E:/Projects/Embed/STM32/STM32BasedDigitalBuckConverter/STM32BasedDigitalBuckConverter/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_dma.c" "E:/Projects/Embed/STM32/STM32BasedDigitalBuckConverter/STM32BasedDigitalBuckConverter/cmake-build-debug/CMakeFiles/BuckConverter.elf.dir/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_dma.c.obj"
+  "E:/Projects/Embed/STM32/STM32BasedDigitalBuckConverter/STM32BasedDigitalBuckConverter/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_exti.c" "E:/Projects/Embed/STM32/STM32BasedDigitalBuckConverter/STM32BasedDigitalBuckConverter/cmake-build-debug/CMakeFiles/BuckConverter.elf.dir/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_exti.c.obj"
+  "E:/Projects/Embed/STM32/STM32BasedDigitalBuckConverter/STM32BasedDigitalBuckConverter/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_flash.c" "E:/Projects/Embed/STM32/STM32BasedDigitalBuckConverter/STM32BasedDigitalBuckConverter/cmake-build-debug/CMakeFiles/BuckConverter.elf.dir/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_flash.c.obj"
+  "E:/Projects/Embed/STM32/STM32BasedDigitalBuckConverter/STM32BasedDigitalBuckConverter/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_flash_ex.c" "E:/Projects/Embed/STM32/STM32BasedDigitalBuckConverter/STM32BasedDigitalBuckConverter/cmake-build-debug/CMakeFiles/BuckConverter.elf.dir/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_flash_ex.c.obj"
+  "E:/Projects/Embed/STM32/STM32BasedDigitalBuckConverter/STM32BasedDigitalBuckConverter/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_gpio.c" "E:/Projects/Embed/STM32/STM32BasedDigitalBuckConverter/STM32BasedDigitalBuckConverter/cmake-build-debug/CMakeFiles/BuckConverter.elf.dir/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_gpio.c.obj"
+  "E:/Projects/Embed/STM32/STM32BasedDigitalBuckConverter/STM32BasedDigitalBuckConverter/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_gpio_ex.c" "E:/Projects/Embed/STM32/STM32BasedDigitalBuckConverter/STM32BasedDigitalBuckConverter/cmake-build-debug/CMakeFiles/BuckConverter.elf.dir/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_gpio_ex.c.obj"
+  "E:/Projects/Embed/STM32/STM32BasedDigitalBuckConverter/STM32BasedDigitalBuckConverter/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_pwr.c" "E:/Projects/Embed/STM32/STM32BasedDigitalBuckConverter/STM32BasedDigitalBuckConverter/cmake-build-debug/CMakeFiles/BuckConverter.elf.dir/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_pwr.c.obj"
+  "E:/Projects/Embed/STM32/STM32BasedDigitalBuckConverter/STM32BasedDigitalBuckConverter/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_rcc.c" "E:/Projects/Embed/STM32/STM32BasedDigitalBuckConverter/STM32BasedDigitalBuckConverter/cmake-build-debug/CMakeFiles/BuckConverter.elf.dir/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_rcc.c.obj"
+  "E:/Projects/Embed/STM32/STM32BasedDigitalBuckConverter/STM32BasedDigitalBuckConverter/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_rcc_ex.c" "E:/Projects/Embed/STM32/STM32BasedDigitalBuckConverter/STM32BasedDigitalBuckConverter/cmake-build-debug/CMakeFiles/BuckConverter.elf.dir/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_rcc_ex.c.obj"
+  "E:/Projects/Embed/STM32/STM32BasedDigitalBuckConverter/STM32BasedDigitalBuckConverter/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_tim.c" "E:/Projects/Embed/STM32/STM32BasedDigitalBuckConverter/STM32BasedDigitalBuckConverter/cmake-build-debug/CMakeFiles/BuckConverter.elf.dir/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_tim.c.obj"
+  "E:/Projects/Embed/STM32/STM32BasedDigitalBuckConverter/STM32BasedDigitalBuckConverter/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_tim_ex.c" "E:/Projects/Embed/STM32/STM32BasedDigitalBuckConverter/STM32BasedDigitalBuckConverter/cmake-build-debug/CMakeFiles/BuckConverter.elf.dir/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_tim_ex.c.obj"
+  "E:/Projects/Embed/STM32/STM32BasedDigitalBuckConverter/STM32BasedDigitalBuckConverter/Src/main.c" "E:/Projects/Embed/STM32/STM32BasedDigitalBuckConverter/STM32BasedDigitalBuckConverter/cmake-build-debug/CMakeFiles/BuckConverter.elf.dir/Src/main.c.obj"
+  "E:/Projects/Embed/STM32/STM32BasedDigitalBuckConverter/STM32BasedDigitalBuckConverter/Src/stm32f1xx_hal_msp.c" "E:/Projects/Embed/STM32/STM32BasedDigitalBuckConverter/STM32BasedDigitalBuckConverter/cmake-build-debug/CMakeFiles/BuckConverter.elf.dir/Src/stm32f1xx_hal_msp.c.obj"
+  "E:/Projects/Embed/STM32/STM32BasedDigitalBuckConverter/STM32BasedDigitalBuckConverter/Src/stm32f1xx_it.c" "E:/Projects/Embed/STM32/STM32BasedDigitalBuckConverter/STM32BasedDigitalBuckConverter/cmake-build-debug/CMakeFiles/BuckConverter.elf.dir/Src/stm32f1xx_it.c.obj"
+  "E:/Projects/Embed/STM32/STM32BasedDigitalBuckConverter/STM32BasedDigitalBuckConverter/Src/syscalls.c" "E:/Projects/Embed/STM32/STM32BasedDigitalBuckConverter/STM32BasedDigitalBuckConverter/cmake-build-debug/CMakeFiles/BuckConverter.elf.dir/Src/syscalls.c.obj"
+  "E:/Projects/Embed/STM32/STM32BasedDigitalBuckConverter/STM32BasedDigitalBuckConverter/Src/sysmem.c" "E:/Projects/Embed/STM32/STM32BasedDigitalBuckConverter/STM32BasedDigitalBuckConverter/cmake-build-debug/CMakeFiles/BuckConverter.elf.dir/Src/sysmem.c.obj"
+  "E:/Projects/Embed/STM32/STM32BasedDigitalBuckConverter/STM32BasedDigitalBuckConverter/Src/system_stm32f1xx.c" "E:/Projects/Embed/STM32/STM32BasedDigitalBuckConverter/STM32BasedDigitalBuckConverter/cmake-build-debug/CMakeFiles/BuckConverter.elf.dir/Src/system_stm32f1xx.c.obj"
+  )
+set(CMAKE_C_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_C
+  "DEBUG"
+  "STM32F103x6"
+  "USE_HAL_DRIVER"
+  )
+
+# The include file search paths:
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  "../Inc"
+  "../Drivers/STM32F1xx_HAL_Driver/Inc"
+  "../Drivers/STM32F1xx_HAL_Driver/Inc/Legacy"
+  "../Drivers/CMSIS/Device/ST/STM32F1xx/Include"
+  "../Drivers/CMSIS/Include"
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ STM32BasedDigitalBuckConverter/cmake-build-debug/CMakeFiles/BuckConverter.elf.dir/cmake_clean.cmake
@@ -0,0 +1,30 @@
+file(REMOVE_RECURSE
+  "BuckConverter.elf"
+  "BuckConverter.elf.manifest"
+  "BuckConverter.elf.pdb"
+  "CMakeFiles/BuckConverter.elf.dir/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal.c.obj"
+  "CMakeFiles/BuckConverter.elf.dir/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_cortex.c.obj"
+  "CMakeFiles/BuckConverter.elf.dir/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_dma.c.obj"
+  "CMakeFiles/BuckConverter.elf.dir/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_exti.c.obj"
+  "CMakeFiles/BuckConverter.elf.dir/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_flash.c.obj"
+  "CMakeFiles/BuckConverter.elf.dir/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_flash_ex.c.obj"
+  "CMakeFiles/BuckConverter.elf.dir/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_gpio.c.obj"
+  "CMakeFiles/BuckConverter.elf.dir/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_gpio_ex.c.obj"
+  "CMakeFiles/BuckConverter.elf.dir/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_pwr.c.obj"
+  "CMakeFiles/BuckConverter.elf.dir/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_rcc.c.obj"
+  "CMakeFiles/BuckConverter.elf.dir/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_rcc_ex.c.obj"
+  "CMakeFiles/BuckConverter.elf.dir/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_tim.c.obj"
+  "CMakeFiles/BuckConverter.elf.dir/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_tim_ex.c.obj"
+  "CMakeFiles/BuckConverter.elf.dir/Src/main.c.obj"
+  "CMakeFiles/BuckConverter.elf.dir/Src/stm32f1xx_hal_msp.c.obj"
+  "CMakeFiles/BuckConverter.elf.dir/Src/stm32f1xx_it.c.obj"
+  "CMakeFiles/BuckConverter.elf.dir/Src/syscalls.c.obj"
+  "CMakeFiles/BuckConverter.elf.dir/Src/sysmem.c.obj"
+  "CMakeFiles/BuckConverter.elf.dir/Src/system_stm32f1xx.c.obj"
+  "CMakeFiles/BuckConverter.elf.dir/Startup/startup_stm32f103c6tx.s.obj"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang ASM C)
+  include(CMakeFiles/BuckConverter.elf.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
